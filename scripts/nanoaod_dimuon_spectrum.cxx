763e76459dab8ad933e6f10a9823589ff6529270
Stefan Wunsch <stefan.wunsch@cern.ch>
2018-08-24 14:03:56 +0200

    nice up program running the analysis with tutorial header and comments

--- scripts/nanoaod_dimuon_spectrum.cxx
+++ scripts/nanoaod_dimuon_spectrum.cxx
@@ -1,10 +1,24 @@
+/// \file
+/// \ingroup tutorial_dataframe
+/// \notebook -nodraw
+/// This tutorial illustrates how NanoAOD files can be processed with ROOT
+/// dataframes. The NanoAOD-like input file is filled with events from
+/// CMS OpenData containing Muon candidates from 2011 data. The script matches
+/// Muon pairs and produces an histogram of the Di-Muon mass spectrum showing
+/// particle resonances up to the Z mass.
+///
+/// \macro_code
+///
+/// \date August 2018
+/// \author Stefan Wunsch
+
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RVec.hxx"
 #include "TCanvas.h"
 #include "TH1D.h"
+#include "TLatex.h"
 #include "TLorentzVector.h"
 #include "TStyle.h"
-#include "TLatex.h"
 
 using namespace ROOT::VecOps;
 
@@ -17,47 +31,50 @@ void invariant_dimuon_mass() {
   // Create dataframe from NanoAOD file
   ROOT::RDataFrame df("Events", "NanoAOD_Example_CMS2011OpenData.root");
 
-  // Baseline selection of more than two Muons
+  // Baseline selection of more than two muons
   auto df_filtered = df.Filter("nMuon>=2", "More than two muons");
 
-  // Select Muon pair with highest pt and opposite charge
-  auto find_good_pair = [](RVec<double> &pt, RVec<double> &charge) {
+  // Fin muon pair with highest pt and opposite charge
+  auto find_pair = [](RVec<double> &pt, RVec<double> &charge) {
+    // Get indices that sort the muon pts in descending order
     auto idx = Reversed(Argsort(pt));
-    RVec<size_t> muon_good;
+
+    // Find muon with second-highest pt and opposite charge
     for (size_t i = 1; i < idx.size(); i++) {
       const auto i1 = idx[0];
       const auto i2 = idx[i];
       if (charge[i1] != charge[i2]) {
-        muon_good.push_back(i1);
-        muon_good.push_back(i2);
-        break;
+        return RVec<size_t>({i1, i2});
       }
     }
-    return muon_good;
-  };
-  auto df_good =
-      df_filtered
-          .Define("Muon_good", find_good_pair, {"Muon_pt", "Muon_charge"})
-          .Filter("Muon_good.size() == 2", "Found valid pair");
 
-  // Calculate invariant mass of Di-Muon systems
+    // Return empty selection if no candidate matches
+    return RVec<size_t>({});
+  };
+  auto df_pair =
+      df_filtered.Define("Muon_pair", find_pair, {"Muon_pt", "Muon_charge"})
+          .Filter("Muon_pair.size() == 2", "Found valid pair");
+
+  // Compute invariant mass of the di-muon system
   auto compute_mass = [](RVec<double> &pt, RVec<double> &eta, RVec<double> &phi,
                          RVec<double> &mass, RVec<size_t> &idx) {
+    // Compose four-vectors of both muons
     TLorentzVector p1;
-    TLorentzVector p2;
     const auto i1 = idx[0];
-    const auto i2 = idx[1];
     p1.SetPtEtaPhiM(pt[i1], eta[i1], phi[i1], mass[i1]);
-    p2.SetPtEtaPhiM(pt[i2], eta[i2], phi[i2], mass[i2]);
-    auto p = p1 + p2;
-    auto dimuon_mass = p.M();
-    return dimuon_mass;
-  };
-  auto df_mass = df_good.Define(
-      "Dimuon_mass", compute_mass,
-      {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_good"});
 
-  // Plot histogram of Di-Muon mass spectrum
+    TLorentzVector p2;
+    const auto i2 = idx[1];
+    p2.SetPtEtaPhiM(pt[i2], eta[i2], phi[i2], mass[i2]);
+
+    // Add four-vectors to build di-muon system and return the invariant mass
+    return (p1 + p2).M();
+  };
+  auto df_mass = df_pair.Define(
+      "Dimuon_mass", compute_mass,
+      {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_pair"});
+
+  // Plot histogram of di-muon mass spectrum
   auto h = df_mass.Histo1D({"Dimuon_mass", "Dimuon_mass", 20000, 0.25, 300},
                            "Dimuon_mass");
 
@@ -77,7 +94,8 @@ template <typename T> void Draw(T h_result_ptr) {
   auto h = h_result_ptr.GetValue();
   h.SetTitle("");
   h.GetXaxis()->SetTitle("Invariant Di-Muon mass (GeV)");
-  h.GetXaxis()->SetTitleOffset(1.2);;
+  h.GetXaxis()->SetTitleOffset(1.2);
+  ;
   h.GetYaxis()->SetTitle("N_{events}");
   h.Draw();
 
